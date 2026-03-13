90fc3e5adaa797142ba9627c2871c1b50b93b0b3
bhagyakanumuri <bhagyakanumuri127@gmail.com>
2026-03-13 19:17:09 +0530

    Add language switcher to navbar

--- farmprice/urls.py
+++ farmprice/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import set_language
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('i18n/setlang/', set_language, name='set_language'),
     path('', include('crops.urls')),
 ]
