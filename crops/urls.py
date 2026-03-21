56cee006fc7d258341305fe0aab932034bcf6e38
bhagyakanumuri <bhagyakanumuri127@gmail.com>
2026-03-21 14:34:08 +0530

    Add landing page, login required for home

--- crops/urls.py
+++ crops/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.landing, name='landing'),
+    path('home/', views.home, name='home'),
     path('prices/', views.crop_prices, name='crop_prices'),
     path('predict/', views.price_prediction, name='price_prediction'),
     path('weather/', views.weather_advice, name='weather_advice'),
